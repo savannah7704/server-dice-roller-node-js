f350fda5205de226252d9c865baf354562331566
savannah7704 <savannah7704@comcast.net>
2025-02-28 19:26:30 -0600

    Updated files

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-/*app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, 'static')));
 const allowedOrigin = 'https://polite-island-01e695b10.4.azurestaticapps.net';
 app.use(cors({
   origin: allowedOrigin
-}));*/
+}));
 
 app.get('/roll', (request, response) => {
     console.log('Calling "/roll" on the Node.js server.');
